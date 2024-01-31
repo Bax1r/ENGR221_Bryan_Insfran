9e6169469b7e6b78aaa3687667c3968cc74e46a7
Bax1r <bryaninsfranu3u@gmail.com>
2024-01-31 11:08:45 -0800

    Lab 1 Commit

---
+++ Lab1.py
@@ -0,0 +1,10 @@
+"""
+Author: Bryan Insfran
+File name: Lab1.py
+Description: Store a message in a variable then use the print function to call on the variable
+Date: 1/31/2024
+"""
+
+message = "Go Gators"   # Storing the string "Go Gators" in a variable called message
+
+print(message)          # Using the print function to call on the message variable
